061da7d78e87e5cb2724ed5535c44299a857b7f1
Darien <dweems@andrew.cmu.edu>
2018-02-16 02:53:38 -0500

    JSON Formatting
    Reformatted JSON structure in test files

--- test_json_utils.py
+++ test_json_utils.py
@@ -6,13 +6,12 @@ import json
 def make_game_library_from_json( json_data ):
     #Initialize a new GameLibrary
     game_library = test_data.GameLibrary()
-    games = json_data["games"]
-    for gameData in games:
+    for gameData in json_data["games"]:
         game = test_data.Game()
-        game.title = games[gameData]["title"]
-        game.platform = test_data.Platform(games[gameData]["platform"]["name"], 
-                                           games[gameData]["platform"]["launch year"])
-        game.year = games[gameData]["year"]
+        game.title = gameData["title"]
+        game.platform = test_data.Platform(gameData["platform"]["name"], 
+                                           gameData["platform"]["launch year"])
+        game.year = gameData["year"]
         game_library.add_game(game)
     #Loop through the json_data
         #Create a new Game object from the json_data by reading
